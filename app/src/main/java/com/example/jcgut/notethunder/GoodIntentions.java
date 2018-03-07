39ce049d019ffd36c65e80b916645c33a2484218
MarcelloCarlo Sa Desktop <hindot@github.com>
2018-03-08 02:59:44 +0800

    Lockscreen Inflation Success

---
+++ app/src/main/java/com/example/jcgut/notethunder/GoodIntentions.java
@@ -0,0 +1,20 @@
+package com.example.jcgut.notethunder;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by jcgut on 3/8/2018.
+ */
+
+public class GoodIntentions extends BroadcastReceiver {
+    public GoodIntentions() {
+
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        context.startService(new Intent(context, LockScreenMemontumService.class));
+    }
+}
